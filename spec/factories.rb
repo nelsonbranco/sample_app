9367bac1feed4199dc08b115cecb98c27bcae6e6
nelsonbranco <nelsonbranco1977@gmail.com>
2012-07-23 12:09:28 +0100

    finish the signup

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Michael Hartl"
+    email    "nelsonbranco1977@gmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
+    
